19661dfb9a029239a868b47ae7ee5bd3627fe93b
humphs078 <sean@cruaghgreen.com>
2023-03-30 07:57:30 +0100

    Analysis.py - read in iris.data set using pandas and print to test

--- analysis.py
+++ analysis.py
@@ -1,3 +1,13 @@
 # Analysis.py
 # Author: Sean Humphreys
-# Script to read in the IRIS data set
+# Script to read in and analyse the IRIS data set
+
+# import the pandas module used to read in the data set file as a data frame
+import pandas as pd
+
+
+# Define data frame as variable df to read in file 'iris.data' with a separator ',' and the column names as defined in
+# the the list variable called "names"
+# Reference https://gist.github.com/curran/a08a1080b88344b0c8a7#file-iris-csv - accessed 30/03/2023
+df = pd.read_csv('iris.data', sep=',', names=["sepal_length", "sepal_width", "petal_length", "petal_width", "class"])
+print(df)  # validation test
