9ebb27b30a80369ea6fd3c35ec7e1afeef02e08b
humphs078 <sean@cruaghgreen.com>
2023-03-31 12:02:16 +0100

    Update rough work to get better table summarising data

--- rough_work.py
+++ rough_work.py
@@ -3,12 +3,14 @@
 # This file is for testing different approaches
 
 import pandas as pd
+import csv
 from pandas.plotting import table
 import plotly.figure_factory as ff
+import plotly.express as px
 import seaborn as sns
 sns.set_theme(context='notebook', style='darkgrid', palette='pastel', font='sans-serif', font_scale=1, color_codes=True, rc=None)
 import matplotlib.pyplot as plt
-import numpy as py
+import numpy as np
 
 # download iris data and read it into a dataframe
 # url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
@@ -16,63 +18,31 @@ import numpy as py
 # print(df)
 df = pd.read_csv('iris.data', sep=',', names=["Sepal Length cm", "Sepal Width cm", "Petal Length cm",
                                               "Petal Width cm", "Species"])
-# fig = ff.create_table(df)
-# fig.update_layout(autosize=True)
-# fig.write_image("table_plotly.png", scale=1)
-
-# specify path for export
-# path = r'data_summary.txt'
-
-# export DataFrame to text file
-# with open(path, 'a') as f:
-#    df_string = df.to_string(header=True, index=False)
-#    f.write(df_string)
-
-#fig, axes = plt.subplots(2, 2, figsize=(10, 10))
-
-#axes[0, 0].set_title("Sepal Length")
-#axes[0, 0].hist(df['sepal_length_cm'], bins=7)
-
-#axes[0, 1].set_title("Sepal Width")
-#axes[0, 1].hist(df['sepal_width_cm'], bins=5);
-
-#axes[1, 0].set_title("Petal Length")
-#axes[1, 0].hist(df['petal_length_cm'], bins=6);
-
-#axes[1, 1].set_title("Petal Width")
-#axes[1, 1].hist(df['petal_width_cm'], bins=6);
-
-#plot = sns.FacetGrid(df, hue="Species")
-#plot.map(sns.histplot, "Sepal Length cm", kde=True).add_legend()
-#plt.savefig('images/plots/histograms/sepal_length_histogram.png')
-
-#plot = sns.FacetGrid(df, hue="Species")
-#plot.map(sns.histplot, "Sepal Width cm", kde=True).add_legend()
-#plt.savefig('images/plots/histograms/sepal_width_histogram.png')
-
-#plot = sns.FacetGrid(df, hue="Species")
-#plot.map(sns.histplot, "Petal Length cm", kde=True).add_legend()
-#plt.savefig('images/plots/histograms/petal_length_histogram.png')
-
-#plot = sns.FacetGrid(df, hue="Species")
-#plot.map(sns.histplot, "Petal Width cm", kde=True).add_legend()
-#plt.savefig('images/plots/histograms/petal_width_histogram.png')
-#plt.show()
-
-#mini = df.min(
-df.max()
-df.mean(numeric_only=True)
-df.median(numeric_only=True)
-df.std(numeric_only=True)
-#print(mini)
-
-
 
 summary = df.describe()
-#print(summary)
+summary_round = df.describe().T
+print(summary_round.head())
 
-ax = plt.subplot(111, frame_on=False) # no visible frame
-ax.xaxis.set_visible(False) # hide the x axis
-ax.yaxis.set_visible(False) # hide the y axis
-table(ax, summary, loc='center') # where df is your data frame
-plt.savefig('mytable.png')
+summary_round.transpose().to_csv('summary stats.csv', sep=',')
+
+# adding header
+header = [' ', 'Sepal Length cm', 'Sepal Width cm', 'Petal Length cm', 'Petal Width cm']
+
+with open('summary stats.csv', 'r') as fp:
+    reader = csv.DictReader(fp, fieldnames=header)
+
+    # use newline='' to avoid adding new CR at end of line
+    with open('output.csv', 'w', newline='') as fh:
+        writer = csv.DictWriter(fh, fieldnames=reader.fieldnames)
+        writer.writeheader()
+        header_mapping = next(reader)
+        writer.writerows(reader)
+
+
+data_summary = pd.read_csv('output.csv', sep=',')
+
+fig2 = ff.create_table(data_summary)
+#  method to change the look and feel of the table
+fig2.update_layout(autosize=False, width=700, height=200)
+# write the table_plotly.png file to the images folder
+fig2.write_image("images/tables/data_summary.png", scale=1)
